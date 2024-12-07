7a314e787386791cfa5892a31b79286fc116c131
Joel DuBien <joel@peakmetrics.com>
2024-12-07 08:37:52 -0700

    use docker tag

--- postgres-util/pgdump.sh
+++ postgres-util/pgdump.sh
@@ -1,9 +1,8 @@
 #!/usr/bin/env bash
 
-username="$1"
-password="$2"
-host="$3"
-db="$4"
+username="$POSTGRES_USER"
+host="$POSTGRES_HOST"
+db="$POSTGRES_DB"
 
 pg_dump -U $username -h $host --clean $db | gzip > dump.sql.gz
 sleep 300 # 5 mins timeout
