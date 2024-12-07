394252eb6af91624b77889e37889e4c9cf0e1fc7
Joel DuBien <joel@peakmetrics.com>
2024-12-07 09:01:44 -0700

    clean is an arg

--- postgres-util/pgdump.sh
+++ postgres-util/pgdump.sh
@@ -3,6 +3,7 @@
 username="$POSTGRES_USER"
 host="$POSTGRES_HOST"
 db="$POSTGRES_DB"
+clean="${POSTGRES_CLEAN:-}"
 
-pg_dump -U $username -h $host --clean $db | gzip > dump.sql.gz
+pg_dump -U $username -h $host $clean $db | gzip > dump.sql.gz
 sleep 300 # 5 mins timeout
